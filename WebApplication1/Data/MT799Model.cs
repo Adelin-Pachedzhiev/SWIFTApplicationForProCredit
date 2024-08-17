9c8fefa1f64f5d8d64a6261b79b2815557d90be6
erolclk1 <erol.aybar@gmail.com>
2024-08-17 17:21:50 +0300

    Work in progress

---
+++ WebApplication1/Data/MT799Model.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Data
+{
+    public class MT799Model
+    {
+        public string BasicHeader { get; set; }
+        public string ApplicationHeader { get; set; }
+        public string UserHeader { get; set; }
+        public string Text { get; set; }
+        public string Tailers { get; set; }
+    }
+}
---
+++ WebApplication1/Services/ISwiftParserService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Data;
+
+namespace WebApplication1.Services
+{
+    public interface ISwiftParserService
+    {
+        MT799Model Parser(string swiffContent);
+    }
+}
